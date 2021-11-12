c6fb098b53d2ab3c645f42d36bb6181f2f6e275e
Arkanic <silverfishasaurus@gmail.com>
2021-11-12 20:10:24 +0000

    and that

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ app.get("/stars", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || "3000", () => {
+app.listen(process.env.PORT || "8080", () => {
     console.log("Server Started");
 });
